9f82c89dc258b898f75e2c8f8e3235013608c7f0
Aymen Awol <aymenawol070106@gmail.com>
2026-02-20 22:29:41 -0800

    fix marc stupid error

--- src/app/customer/join/[token]/page.tsx
+++ src/app/customer/join/[token]/page.tsx
@@ -62,7 +62,7 @@ export default function CustomerJoinPage() {
         .from("customers")
         .select("id")
         .eq("auth_user_id", userId)
-        .single();w
+        .single();
 
       if (existingCustomer) {
         customerId = existingCustomer.id;
